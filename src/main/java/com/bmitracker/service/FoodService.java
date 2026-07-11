abfd63fa700d254ea0b83c4a9d34cbf4804b1ef9
NiLuo <1656249300@qq.com>
2026-07-11 16:52:35 +0800

    Delete src/main/java/com/bmitracker directory

--- src/main/java/com/bmitracker/service/FoodService.java
+++
@@ -1,9 +0,0 @@
-package com.bmitracker.service;
-
-import com.bmitracker.model.Food;
-import java.util.List;
-
-public interface FoodService {
-    List<Food> getAllFoods();
-    List<Food> getFoodsByCategory(String category);
-}
